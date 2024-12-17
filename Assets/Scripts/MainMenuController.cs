8b04d6d0ed0e34632af2d0bf9e15211ffa1cb855
Kenzyyounis <135533701+Kenzyyounis@users.noreply.github.com>
2024-12-17 21:02:34 +0200

    MainMenuFunctionality

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,13 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    // Reference to the instruction page (UI Panel)
+    public GameObject instructionPage;
+
+    public GameObject StartButton;
+    public GameObject InstructButton;
+    public GameObject QuitButton;
+
     public void StartGame()
     {
-        // Replace "GameScene" with the name of your gameplay scene
+        // Replace "EndlessRunner" with the name of your gameplay scene
         SceneManager.LoadScene("EndlessRunner");
     }
 
@@ -15,6 +21,36 @@ public class MainMenuController : MonoBehaviour
     {
         // This is a placeholder for settings functionality
         Debug.Log("Settings menu opened!");
+    }
+
+    // Open Instructions (Show instruction page)
+    public void OpenInstructions()
+    {
+        // Show the instruction page
+        instructionPage.SetActive(true);
+
+        // Hide other buttons
+        StartButton.SetActive(false);
+        InstructButton.SetActive(false);
+        QuitButton.SetActive(false);
+
+        // Log to the console
+        Debug.Log("Instructions page opened!");
+    }
+
+    // Close Instructions (Hide instruction page)
+    public void CloseInstructions()
+    {
+        // Hide the instruction page
+        instructionPage.SetActive(false);
+
+        // Show other buttons
+        StartButton.SetActive(true);
+        InstructButton.SetActive(true);
+        QuitButton.SetActive(true);
+
+        // Log to the console
+        Debug.Log("Instructions page closed!");
     }
 
     public void QuitGame()
